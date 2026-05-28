f4ab5b04542def51c0c63043a86385724b5e6c92
Victor Eduardo Serrato Lanuza <victor.serrato@globant.com>
2026-05-28 09:08:52 -0500

    Add block collection

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -186,4 +186,9 @@ async function loadPage() {
   loadDelayed();
 }
 
+(async function loadDa() {
+  if (!new URL(window.location.href).searchParams.get('dapreview')) return;
+  import('https://da.live/scripts/dapreview.js').then(({ default: daPreview }) => daPreview(loadPage));
+}());
+
 loadPage();
